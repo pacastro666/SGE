cb83698de030166eeae371d168d6210513ed2939
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-10-10 23:46:36 -0300

    adicionado cadastro e listagem de marcas

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.db.models.query import QuerySet
+from django.views.generic import ListView,CreateView
+from . import models,forms
+
+
+class BrandListView(ListView):
+    model = models.Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
+
+    def get_queryset(self) :
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = 'brand_create.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
