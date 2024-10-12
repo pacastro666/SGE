893391ee601f4652e1a0844be28d6214275a400e
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-10-11 23:44:37 -0300

    CRUD COMPLETO DE MARCAS

--- brands/views.py
+++ brands/views.py
@@ -1,7 +1,7 @@
 
 from django.urls import reverse_lazy
 from django.db.models.query import QuerySet
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,DetailView,UpdateView,DeleteView
 from . import models,forms
 
 
@@ -9,6 +9,7 @@ class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+    paginate_by = 3
 
     def get_queryset(self) :
         queryset = super().get_queryset()
@@ -24,3 +25,19 @@ class BrandCreateView(CreateView):
     template_name = 'brand_create.html'
     form_class = forms.BrandForm
     success_url = reverse_lazy('brand_list')
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = 'brand_detail.html'
+
+class BrandUpdateView(UpdateView):
+    model = models.Brand
+    template_name = 'brand_update.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
+    success_url = reverse_lazy('brand_list')
+
