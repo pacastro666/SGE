6d75f4fd61df6e4d98cfbb6edc0fef173440dfac
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-10-19 14:36:34 -0300

    melhorando o filtro de produtos

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView,CreateView,DetailView,UpdateView,DeleteView
 from . import models,forms
-
+from categories.models import Category
+from brands.models import Brand
 
 class ProductListView(ListView):
     model = models.Product
@@ -12,11 +13,26 @@ class ProductListView(ListView):
     def get_queryset(self) :
         queryset = super().get_queryset()
         title = self.request.GET.get('title')
+        category = self.request.GET.get('category')
+        brand = self.request.GET.get('brand')
 
         if title:
             queryset = queryset.filter(title__icontains=title)
+        
+        if category:
+            queryset = queryset.filter(category_id=category)
+            
+        
+        if brand:
+            queryset = queryset.filter(brand_id=brand)
 
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['brands'] = Brand.objects.all()
+        return context
 
 class ProductCreateView(CreateView):
     model = models.Product
