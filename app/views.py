035833cb568728bd7bef9d47207bb577ea9ab06a
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-10-22 20:02:09 -0300

    gerando metricas de vendas

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from . import metrics
 
 
 def home(request):
-    context = {}
+    product_metrics = metrics.get_product_metrics()
+    seles_metrics = metrics.get_sales_metrics()
+    context = {
+        'product_metrics': product_metrics,
+        'sales_metrics':seles_metrics,
+    }
     return render(request, 'home.html', context)
