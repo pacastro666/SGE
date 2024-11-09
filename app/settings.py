2e9c187431233776ee47ac106b9075ddf924ef6a
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-11-09 11:56:55 -0300

    criando as rotas de authenticação jwt

--- app/settings.py
+++ app/settings.py
@@ -147,6 +147,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
          'rest_framework.permissions.IsAuthenticated',
+         'rest_framework.permissions.DjangoModelPermissions',
     ),
 }
 
